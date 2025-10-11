57b409182d4502f4bffda61c947ea176ba2b28ff
Lgranad13 <leogranadoscastillo13@gmail.com>
2025-10-10 17:37:03 -0700

    Fixed Dupes, self connections, and max users allowed

--- chat.py
+++ chat.py
@@ -11,6 +11,7 @@ import sys
 import threading
 
 connections = []   #initalize array to hold [ (id, sock, ip, port) ]
+MAX_USERS = 3
 LISTEN_PORT = None   # remember which port our server listens on
 
 #IV
@@ -63,26 +64,42 @@ def get_my_ip():
 
 #LG
 #Function #4 Connect -establishes a new TCP connection
-def connect_to(ip, port, self_p):       #takes user ip, port, uses global self_p to hold self port
+def connect_to(ip, port, LISTEN_PORT):       #takes user ip, port, uses global LISTEN_PORT to hold self port
     s = socket.socket()     #create the socket
 
-    selfIP = socket.gethostbyname(socket.gethostname())     #checks ip addr
-    if selfIP == ip and self_p == port:        #if statement to prevent self connections
+    self_IP = get_my_ip()     #checks ip addr
+    if self_IP == ip and LISTEN_PORT == port:        #if statement to prevent self connections
         print("Connection error: Self connection is not allowed")
         return
     
-    for _,_,ip_i,port_i in connections:
+    for _,_,ip_i,port_i in connections:     #for loop to check array for dupes
         if ip_i == ip and port_i == port:
             print("Connection error: Duplicate connections is not allowed")
             return
-    
+        
+    if len(connections) == MAX_USERS:       #if statement to limit connections to 3 max
+        print("Connection error: Reached Max of 3 connections, please terminate one before trying to connect again")
+        s.close()
+        return
+
     try:
-        s.connect((ip, port))       #connect using user input for ip and port
+        s.connect((ip, port))       #connect using user input for ip and port    
+
+        s.settimeout(0.5)  # small timeout to catch server response
+        try:
+            server_msg = s.recv(1024) #check if server sent a message
+            if server_msg: #if there is data
+                print(server_msg.decode().strip())  # Prints connection error server full ...
+                s.close()  # closes the socket
+                return
+        except socket.timeout: #if it times out continues
+            pass
+        s.settimeout(None) #sets the timeout back to none 
         connections.append((len(connections)+1, s, ip, port))       #adds connections to array
         print(f"[chat> [info] Connected to {ip}:{port}")      #notifies user of successful connection
         threading.Thread(target=recv_from, args=(s, ip, port), daemon=True).start() #starts function to keep track of connection
     except Exception as e:
-        print("Connection error:", e)
+        print(f"Connection error: {e}")
 
 #LG
 #Helper function to keep user notified of when connection is terminated/closed
@@ -91,15 +108,19 @@ def recv_from(sock, ip, port):
         try:
             data = sock.recv(1024)      #while loop to check if data connection is still live
             if not data:        #if data is not alive checks connections array to remove and notify user of it closing
-                for i in connections:
-                    if i[2] == ip and i[3] == port:
-                        print(f"[info] Connection closed with {ip}:{port} \n[chat> ", end="")
-                        connections.remove(i)
-                        for i, p in enumerate(connections, start=1):  #goes through the array and changes the id to new reflection
-                            connections[i-1] = (i, p[1], p[2], p[3])
+                
                 break
         except: 
             break
+    
+    for i in connections:
+        if i[2] == ip and i[3] == port:
+            connections.remove(i)
+            print(f"[info] Connection closed with {ip}:{port} \n[chat> ", end="")
+            
+    for i, p in enumerate(connections, start=1):  #goes through the array and changes the id to new reflection
+                            connections[i-1] = (i, p[1], p[2], p[3])
+
     sock.close()        #closes the socket
     
 
@@ -109,8 +130,9 @@ def list_connections():
     if not connections:       #lets user know if nothing is connected
         print("[chat> No connections live.")
     else:               #lets user know of all connections avialabe for chat
+        print(f"    id:  IP address       Port No.")
         for pid, _, ip, port in connections:
-            print(f"{pid}: {ip}:{port}")
+            print(f"    {pid}:  {ip}     {port}")
 
 #LG
 #Function #6 terminate the connection listed under the specified number
@@ -118,11 +140,12 @@ def terminate(pid):
     for i in connections:       #for loop to go through connections array
         if i[0] == pid:         #grabs the id that user inputted and checks if it avialable
             i[1].close()        #closes the socket connections and removes it from the array
-            connections.remove(i)
             print(f"[chat> Terminated {i[2]}:{i[3]}")
+
             for i, p in enumerate(connections, start=1):    #goes through connections array and changes ids to show new reflection
                 connections[i-1] = (i, p[1], p[2], p[3])
             return
+    
     print("[chat> Invalid id")  #notifies user of error
 
 #LG
@@ -133,11 +156,18 @@ def server(port):
     s.listen()
     print(f"Listening on port {port}\n[chat> ", end="")
     while True:     #while loop to accept connections if succesfull
-        c, a = s.accept()
-        pid = len(connections)+1    #gives id to the new connection
-        connections.append((pid, c, a[0], a[1]))        #adds the info to the array connections
-        print(f"[info] New connection from {a[0]}:{a[1]}\n[chat> ", end="")
-        threading.Thread(target=recv_from, args=(c, a[0], a[1]), daemon=True).start()   #starts the thread to keep track of connection
+            c, a = s.accept()
+
+            #checks if array is at max
+            if len(connections) >= MAX_USERS:       #if reached max connections denies connection, notifies client to terminate one, and sends data to user that it is currently full
+                print("Connection error: Reached Max of 3 connections, please terminate one to accept connection \n[chat> ", end="")
+                c.sendall("Connection error: Server is full: connection was denied".encode())
+                c.close()
+            else:
+                pid = len(connections)+1    #gives id to the new connection
+                connections.append((pid, c, a[0], a[1]))        #adds the info to the array connections
+                print(f"[info] New connection from {a[0]}:{a[1]}\n[chat> ", end="")
+                threading.Thread(target=recv_from, args=(c, a[0], a[1]), daemon=True).start()   #starts the thread to keep track of connection
 
 #LG
 #Main program to continously run until exited/closed
@@ -147,7 +177,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     LISTEN_PORT = int(sys.argv[1])
-    threading.Thread(target=server, args=(LISTEN_PORT,), daemon=True).start()
+    threading.Thread(target=server, args=(LISTEN_PORT,), daemon=True).start()   #Theard to start the server
 
     #while loop to keep checking for user input
     while True:
@@ -155,7 +185,7 @@ if __name__ == "__main__":
         if not cmd: 
             continue
         if cmd[0]=="connect" and len(cmd)==3:           #4 if connect is selected runs connect function
-            connect_to(cmd[1], int(cmd[2]))
+            connect_to(cmd[1], int(cmd[2]), LISTEN_PORT)
         elif cmd[0] == "help":                          #1 if help is selected runs help function
             show_help()
         elif cmd[0] == "myip":                          #2 if myip is selected runs myip function
@@ -166,4 +196,4 @@ if __name__ == "__main__":
             list_connections()
         elif cmd[0]=="terminate" and len(cmd)==2:       #6 if terminated is selected runs the terminated function
             terminate(int(cmd[1]))
-        else: print("Command does not exist.")  #to let user know command doesn't exist
+        else: print("Command does not exist. Please type - help - for commands")  #to let user know command doesn't exist
